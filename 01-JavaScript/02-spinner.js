1bfa075e0c0f73e3979ff691b48b795709cd9d3c
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-30 18:41:46 +0530

    closures demo

--- 01-JavaScript/02-spinner.js
+++ 01-JavaScript/02-spinner.js
@@ -15,6 +15,63 @@ var spinner = (function(){
     }
 })();
 
+function Spinner(){
+    var counter = 0;
+
+    this.up = function up() {
+        return ++counter;
+    };
+
+    this.down = function down() {
+        return --counter;
+    };
+}
+
+function Spinner() {
+    this.__counter__ = 0;
+}
+
+Spinner.prototype.up = function up() {
+    return ++this.__counter__;
+};
+
+Spinner.prototype.down = function down() {
+    return --this.__counter__;
+};
+
+class Spinner{
+    counter = 0;
+
+    up(){
+        return ++this.counter;
+    }
+
+    down(){
+        return --this.counter;
+    }
+}
+
+
+var Spinner = (function(){
+    var counterSymbol = Symbol();
+
+    class Spinner {
+        
+        constructor(){
+            this[counterSymbol] = 0;
+        }
+
+        up() {
+            return ++this[counterSymbol];
+        }
+
+        down() {
+            return --this[counterSymbol];
+        }
+    }
+
+    return Spinner;
+})();
 
 /* 
 up() 
