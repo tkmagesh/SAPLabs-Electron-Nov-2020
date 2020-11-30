926a90c7f8909dc12a4cd1924befaa4d2731394f
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-30 18:17:29 +0530

    spinner implementation

---
+++ 01-JavaScript/02-spinner.js
@@ -0,0 +1,39 @@
+var spinner = (function(){
+    var counter = 0;
+    
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    }
+})();
+
+
+/* 
+up() 
+down()
+*/
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
+
+
+//The following should not be possible
+counter = 1000
+spinner.up()
+
+spinner.counter = 1000
+spinner.up()
