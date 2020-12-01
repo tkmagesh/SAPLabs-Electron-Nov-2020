8b42332ed703063d7179a599fdd1b2fd9719bdb6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-01 13:53:49 +0530

    creating a renderer process

--- 03-first-app/main.js
+++ 03-first-app/main.js
@@ -1,1 +1,27 @@
-console.log('welcome to electron');
+const { BrowserWindow, app } = require('electron');
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration : true
+        }
+    });
+    mainWindow.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
+
+//mac
+app.on('window-all-closed', function(){
+    if (process.platform === 'darwin' /*mac*/){
+        app.quit();
+    }
+});
+
+app.on('activate', function(){
+    if (BrowserWindow.getAllWindows().length === 0){
+        createWindow();
+    }
+})
